ed0a2857d24bb611b127f87077adcd1d0a20a584
YunseoChoe <choeyunseo4060@naver.com>
2024-03-20 16:25:45 +0000

    Update crawling function

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const bodyParser = require('body-parser');        // 미들 웨어
 const cookieparser = require('cookie-parser');    // 미들 웨어
 const cors = require('cors');                     // 미들 웨어
 
+app.use(bodyParser.json());                       // body에 있는 것들을 json으로 파싱 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieparser());                          // cookie값 파싱
+app.use(cors());                                  // cors 보안 
+
 const userController = require('./controller/user.js');
 const chatController = require('./controller/chatGpt.js');
 const crawlController = require('./controller/crawl.js');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieparser());
 
 // 미들 웨어
 function verifyToken(req, res, next) {
@@ -35,7 +36,10 @@ function verifyToken(req, res, next) {
                 return res.status(401).json({message:'TokenFail'});
             }
             else {
-                console.log(decoded.name);
+                // 복호화된 토큰의 내용 확인
+                console.log('이메일: ', decoded.email);
+                console.log('사용자 이름: ', decoded.name);
+                req.email = decoded.email;
                 req.name = decoded.name;
                 next();
             }
@@ -58,9 +62,12 @@ app.post('/signup', userController.signupApi);
 // 로그인 API
 app.post('/login', userController.loginApi);
 
+// 마이페이지 API
+app.get('/mypage', verifyToken, userController.myPageApi);
+
 // test API
 app.post('/test', userController.testApi);
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}!`);
+    console.log(`Server is running at ${port}!`);
 });
